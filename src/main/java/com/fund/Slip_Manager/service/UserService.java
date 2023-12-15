bf91fe17b5b8ef04c4f395deab708f337f6717ab
D_Binara <dbkaveesha@gmail.com>
2023-12-15 23:55:13 +0530

    delete

--- src/main/java/com/fund/Slip_Manager/service/UserService.java
+++ src/main/java/com/fund/Slip_Manager/service/UserService.java
@@ -3,50 +3,33 @@ package com.fund.Slip_Manager.service;
 import com.fund.Slip_Manager.dto.UserDTO;
 import com.fund.Slip_Manager.entity.User;
 import com.fund.Slip_Manager.repo.UserRepo;
-import jakarta.transaction.Transactional;
+
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
+
 
 @Service
 @Transactional
-public class UserService implements UserServiceInterface {
+public class UserService {
+
     @Autowired
     private UserRepo userRepo;
+
     @Autowired
     private ModelMapper modelMapper;
+
     public UserDTO saveUser(UserDTO userDTO){
         userRepo.save(modelMapper.map(userDTO, User.class));
         return userDTO;
     }
-
-    @Override
-    public User findUserByEmail(String email) {
-        return userRepo.findByEmail(email);
-    }
-
-    public List<UserDTO>  getAllUsers(){
+    public List<UserDTO> getAllUsers(){
         List<User>userList=userRepo.findAll();
         return modelMapper.map(userList,new TypeToken<List<UserDTO>>(){}.getType());
     }
-
-
-    public List<UserDTO> findAllUsers() {
-        List<User> users = userRepo.findAll();
-        return users.stream()
-                .map(this::mapToUserDto)
-                .collect(Collectors.toList());
-    }
-
-    private UserDTO mapToUserDto(User user){
-        UserDTO userDto = new UserDTO();
-        String[] str = user.getName().split(" ");
-        userDto.setEmail(user.getEmail());
-        return userDto;
-    }
-
 }
